ae606998fac553f2806c275bc7a39b753f9409c2
Tchoupinax <corentinfiloche@hotmail.fr>
2018-04-20 15:01:05 +0200

    Add MacOS compatibility

--- check-git.sh
+++ check-git.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+linux=true
+if [ $(uname) == 'Darwin' ];then
+    linux=false
+fi
+
 for dir in `find ~/Documents -maxdepth 1 -type d`
 do 
     # Check if directory is a git project
@@ -10,11 +15,18 @@ do
         cd ${dir}
         res=`git status --porcelain`
         # if res is empty, all is clean with git
-        if [ "$res" = "" ]
-        then
-            echo -e "  \e[92m\e[0m\e[94m ${name}\e[0m"
+        if [ "$res" = "" ]; then
+            if $linux; then
+                echo -e "  \e[92m\e[0m\e[94m ${name}\e[0m"
+            else
+                echo $'  \e[92m''' $'\e[94m'$name $'\e[0m'
+            fi
         else
-            echo -e "  \e[91m\e[0m\e[94m ${name}\e[0m"
+            if $linux; then
+                echo -e "  \e[91m\e[0m\e[94m ${name}\e[0m"
+            else
+                echo $'  \e[91m''' $'\e[94m'$name $'\e[0m'
+            fi
         fi
     fi
 done
