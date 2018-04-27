5faa238becd9281e52d5d1a3dc3be064367aded1
Tchoupinax <corentinfiloche@hotmail.fr>
2018-04-27 13:17:31 +0200

    shutdown added

--- server-bashrc-root.sh
+++ server-bashrc-root.sh
@@ -1,5 +1,6 @@
 # For stupid person like me
 alias reboot='echo "Do you know you are a stupid person ? ;)"'
+alias shutdown='echo "Do you know you are a stupid person ? ;)"'
 # Because i'm tired to write ls -la as root
 alias ll='ls -lah'
 # Adding my bash displaying
